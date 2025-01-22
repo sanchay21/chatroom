53e68ab0bc910a517e37fea014b6f9670d899c62
Sanchay Singh <your-new-email@example.comgit config --global user.email sanchaysingh21@gmail.com>
2025-01-22 23:55:08 +0530

    Update admin

--- base/admin.py
+++ base/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Room, Topic, Message
 
+#models uptil now
 admin.site.register(Room)
 admin.site.register(Topic)
 admin.site.register(Message)
