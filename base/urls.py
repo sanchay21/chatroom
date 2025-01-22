0d78672417da570528568a30f414fcec21a58293
Sanchay Singh <your-new-email@example.comgit config --global user.email sanchaysingh21@gmail.com>
2025-01-22 23:53:02 +0530

    Making Models

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 
--- base/views.py
+++ base/views.py
@@ -1,31 +1,30 @@
 from django.shortcuts import render
+from .models import Room
 # from django.http import HttpResponse
 # Create your views here.
 
-rooms = [
-    {
-        'id':1,
-        'name':'Python Chatroom'
-    },
+# rooms = [
+#     {
+#         'id':1,
+#         'name':'Python Chatroom'
+#     },
 
-    {
-        'id':2,
-        'name':'BKC..!!'
-    },
-    {
-        'id':3,
-        'name':'Devlopers...ASSEMBLE !!!'
-    },
-]
+#     {
+#         'id':2,
+#         'name':'BKC..!!'
+#     },
+#     {
+#         'id':3,
+#         'name':'Devlopers...ASSEMBLE !!!'
+#     },
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if(i['id'] == int(pk)):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room':room}
     return render(request, 'base/room.html', context)
